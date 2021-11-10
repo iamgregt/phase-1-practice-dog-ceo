42b34dae205f829bb0169301ab2d2d0780e27e2f
Gregory <taylor.gregory2@gmail.com>
2021-11-09 19:58:58 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+let getDogs = fetch(imgUrl)
+.then(resp => resp.json())
+.then(dogData => dogData.foreach((dog) => console.log(dog)))
+
+    
+//     {
+//     dogs.forEach(dog => {
+//         let pics = document.querySelector('div')
+//     let newDog = document.createElement('img')
+//     newDog.innerHTML = `src=${dog.message}`
+//     pics.appendChild(newDog)
+        
+//     });
+// })
+
+
+// function putOnPage(dog){
+//     let pics = document.querySelector('div')
+//     let newDog = document.createElement('img')
+//     newDog.imgUrl =
+//     pics.appendChild(newDog)
+
+    
+// }
