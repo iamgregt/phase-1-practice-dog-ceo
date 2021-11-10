ccf9af294d9f0edb9840a0100aea5f02f860d03c
Gregory <taylor.gregory2@gmail.com>
2021-11-09 20:55:16 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 let getDogs = fetch(imgUrl)
 .then(resp => resp.json())
-.then(dogData => dogData.foreach((dog) => console.log(dog)))
+.then(data => data.message.forEach(url => putOnPage(url)))
+
+let getBreeds = fetch(breedUrl)
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+
+function putInList(breed){
+    let breedList = document.getElementById('dog-breeds')
+    let newBreed = document.createElement('li')
+    newBreed.innerText = `${breed}`
+    breedList.appendChild(newBreed)
+}
+
+
+function putOnPage(url){
+    let pics = document.querySelector('div')
+    let newDog = document.createElement('li')
+    newDog.innerHTML = `<img src="${url}">`
+    pics.appendChild(newDog)
 
     
-//     {
-//     dogs.forEach(dog => {
-//         let pics = document.querySelector('div')
-//     let newDog = document.createElement('img')
-//     newDog.innerHTML = `src=${dog.message}`
-//     pics.appendChild(newDog)
-        
-//     });
-// })
-
-
-// function putOnPage(dog){
-//     let pics = document.querySelector('div')
-//     let newDog = document.createElement('img')
-//     newDog.imgUrl =
-//     pics.appendChild(newDog)
-
-    
-// }
+}
